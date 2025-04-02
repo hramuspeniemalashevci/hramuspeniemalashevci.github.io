22a3483ea3f6ea0446d65237a5d4f1f5e8267e71
hramuspeniemalashevci <hram.uspenie.malashevci@gmail.com>
2025-04-03 00:37:08 +0300

    lib.js problem

--- GLOBAL/js-global/lib.js
+++ GLOBAL/js-global/lib.js
@@ -1,5 +1,5 @@
-import { html, render } from "/node_modules/lit-html/l/it-html.js";
-// import { html, render } from "../../node_modules/lit-html/lit-html.js";
+// import { html, render } from "/node_modules/lit-html/l/it-html.js";
+import { html, render } from "../../node_modules/lit-html/lit-html.js";
 
 export {
     html,
