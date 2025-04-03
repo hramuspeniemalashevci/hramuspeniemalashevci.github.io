4ceb5b5b8c5eecc94705b32eb5927fdc6dd91093
hramuspeniemalashevci <hram.uspenie.malashevci@gmail.com>
2025-04-04 00:17:16 +0300

    0

--- site/src/lib.js
+++
@@ -1,8 +0,0 @@
-import { html, render } from '../../node/node_modules/lit-html/lit-html.js';
-import page from '../../node/node_modules/page/page.mjs';
-
-export {
-    html,
-    render,
-    page
-};
