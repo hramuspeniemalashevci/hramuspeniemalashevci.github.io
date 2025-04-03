8f0945117996720a2e146b28d822cb1c67f3939f
hramuspeniemalashevci <hram.uspenie.malashevci@gmail.com>
2025-04-04 00:39:28 +0300

    0.0.7

--- video/video-page.js
+++ video/video-page.js
@@ -1,5 +1,4 @@
-// import { html, render } from '../GLOBAL/js-global/lib.js';
-import { html, render } from '../node/node_modules/lit-html/lit-html.js';
+import { html, render } from '../GLOBAL/js-global/lib.js';
 
 const videosData = [
   {
