723f538ddf953841fff495a41b87de6f4fa21e2f
hramuspeniemalashevci <hram.uspenie.malashevci@gmail.com>
2025-04-03 23:14:09 +0300

    a

--- GLOBAL/js-global/lib.js
+++
@@ -1,7 +0,0 @@
-// import { html, render } from '/node_modules/lit-html/l/it-html.js';
-import { html, render } from "../../node_modules/lit-html/lit-html.js";
-
-export {
-    html,
-    render
-};
